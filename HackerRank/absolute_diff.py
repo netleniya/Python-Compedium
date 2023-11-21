a02a472d6a27278a19a4217995ad091a48d2f5aa
Peter Fiee <peterfiee@icloud.com>
2023-11-21 18:28:13 -0500

    initial skeleton

---
+++ HackerRank/absolute_diff.py
@@ -0,0 +1,29 @@
+def minimum_absolute_difference(arr: list[int]) -> int:
+    """
+    Calculate the minimum absolute difference between any two elements in the given list.
+
+    Args:
+        arr (list[int]): A list of integers.
+
+    Returns:
+        int: The minimum absolute difference between any two elements in the list."""
+
+    ...
+
+
+def test_minimum_absolute_difference() -> bool:
+    """
+    Test the minimum_absolute_difference function.
+
+    Returns:
+        bool: True if all the test cases pass, False otherwise."""
+
+    ...
+
+
+def main() -> None:
+    ...
+
+
+if __name__ == "__main__":
+    ...

d49a6894af12b40d8544ee5fa7556de30c20cebb
Peter Fiee <peterfiee@icloud.com>
2023-11-26 12:10:25 -0500

    sourcery refactor

--- HackerRank/find_digits.py
+++ HackerRank/find_digits.py
@@ -5,4 +5,14 @@ def digitize(n: int, base=10):
 
 
 def find_digits(n: int) -> int:
-    return sum(not n % i for i in list(digitize(n)) if i != 0)
+    return sum(1 for i in digitize(n) if i != 0 and n % i == 0)
+
+
+def main() -> None:
+    num = 1012
+    d = find_digits(num)
+    print(f"{d} of the numbers in {num} are perfect divisors")
+
+
+if __name__ == "__main__":
+    main()

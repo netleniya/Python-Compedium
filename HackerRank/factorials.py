538833a2d70569067c7ec1706fee20b836bb9d06
Peter Fiee <peterfiee@icloud.com>
2023-11-26 12:55:09 -0500

    add docstring

--- HackerRank/factorials.py
+++ HackerRank/factorials.py
@@ -1,7 +1,24 @@
+from functools import cache
+
+
+@cache
 def factorial(num: int) -> int:
-    if num < 2:
-        return 1
-    return num * factorial(num - 1)
+    """
+    Calculates the factorial of a given number.
+
+    Args:
+        num (int): The number to calculate the factorial of.
+
+    Returns:
+        int: The factorial of the given number.
+
+    Examples:
+        >>> factorial(0)
+        1
+        >>> factorial(5)
+        120
+    """
+    return 1 if num < 2 else num * factorial(num - 1)
 
 
 def main() -> None:

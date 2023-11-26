fa51ed057fcba286db1a286f0a58dc03d1232e2f
Peter Fiee <peterfiee@icloud.com>
2023-11-26 12:32:13 -0500

    add docstrings

--- HackerRank/find_digits.py
+++ HackerRank/find_digits.py
@@ -1,15 +1,41 @@
 def digitize(n: int, base=10):
+    """
+    Generates the digits of a given number in the specified base.
+
+    Args:
+        n (int): The number to digitize.
+        base (int, optional): The base to use for digitization. Defaults to 10.
+
+    Yields:
+        int: The digits of the number in the specified base.
+    """
     while n:
         n, d = divmod(n, base)
         yield d
 
 
 def find_digits(n: int) -> int:
-    return sum(1 for i in digitize(n) if i != 0 and n % i == 0)
+    """
+    Returns the count of non-zero digits in the given number.
+
+    Args:
+        n (int): The number to find the digits in.
+
+    Returns:
+        int: The count of non-zero digits in the number.
+    """
+    return sum(i != 0 and n % i == 0 for i in digitize(n))
+
+
+def test_find_digits() -> bool:
+    assert find_digits(123456789) == 3
+    assert find_digits(114108089) == 3
+    assert find_digits(110110015) == 6
+    assert find_digits(106108048) == 5
 
 
 def main() -> None:
-    num = 1012
+    num = 110110015
     d = find_digits(num)
     print(f"{d} of the numbers in {num} are perfect divisors")
 

87a08fd1509feac3df2a078f9a5863cef46be519
Peter Fiee <peterfiee@icloud.com>
2023-11-27 04:45:00 -0500

    sourcery refactor and add docstring

--- HackerRank/zigzag.py
+++ HackerRank/zigzag.py
@@ -1,4 +1,18 @@
-def findZigZagSequence(a):
+def findZigZagSequence(a: list[int]) -> None:
+    """
+    Modifies the input list to create a zigzag sequence and prints the elements.
+
+    This function takes a list 'a' as input and modifies it in-place to create a zigzag sequence.
+    The zigzag sequence is created by sorting the list, swapping the middle element with the last element,
+    and then performing a swapping operation between elements from the middle index + 1 to the second-to-last index.
+    Finally, it prints the elements of the modified list.
+
+    Args:
+        a (list): The list of elements to be modified.
+
+    Returns:
+        None
+    """
     a.sort()
     mid = len(a) // 2
     a[mid], a[-1] = a[-1], a[mid]

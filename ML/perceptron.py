93efb9f243c305fd2b6bc309a1c43b0048f46e51
Peter Fiee <peterfiee@icloud.com>
2024-04-10 14:58:10 -0400

    Implement fit and predict methods

--- ML/perceptron.py
+++ ML/perceptron.py
@@ -6,3 +6,24 @@ class Perceptron:
         self.eta: float = eta
         self.epochs: int = epochs
         self.random_state: int = random_state
+
+    def fit(self, X, y):
+        rgen = np.random.default_rng(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        self.errors_ = []
+
+        for _ in range(self.epochs):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0.0, 1, -1)

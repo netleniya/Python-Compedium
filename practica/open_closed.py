2fabc808880995970a556ca062c673ae0d8cd4d6
Peter Fiee <peterfiee@icloud.com>
2024-10-10 22:51:45 -0400

    Problem arises when adding another class.

--- practica/open_closed.py
+++ practica/open_closed.py
@@ -8,18 +8,26 @@ class LiteralText:
         self.text = text
 
 
+class HorizontalLine:
+    def __init__(self, width: int):
+        self.width = width
+
+
 def show_document(doc):
     for element in doc:
         if isinstance(element, Header):
             print(element.text.upper())
         elif isinstance(element, LiteralText):
             print(element.text)
+        elif isinstance(element, HorizontalLine):
+            print("-" * element.width)
 
 
 def main() -> None:
 
     document: list = [
         Header("the open-closed principle"),
+        HorizontalLine(25),
         LiteralText(
             "The open-closed principle states that classes should be open for extension, but closed for modification."
         ),
